ff148699f25803fcd6596b5306aab750aae4dfcf
Maximus Apen <maximus.apen@sofico.be>
2025-06-01 18:53:41 +0100

    Remove licensing comments from Dockerfile, createImages.sh, server.go, and generate_lb_multiport_service.sh for improved readability

--- httpperf/multiport/generate_lb_multiport_service.sh
+++ httpperf/multiport/generate_lb_multiport_service.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# ******************************************************************************
-# * Licensed Materials - Property of IBM
-# * IBM Cloud Kubernetes Service, 5737-D43
-# * (C) Copyright IBM Corp. 2020, 2022 All Rights Reserved.
-# * US Government Users Restricted Rights - Use, duplication or
-# * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-# ******************************************************************************
 
 # Generate httpperf_lb_multiport_service.yaml with 1000 ports starting at 30081
 
