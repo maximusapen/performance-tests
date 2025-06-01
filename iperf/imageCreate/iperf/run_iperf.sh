90a68c3567cf7eeea69760aae3f93ed439d0ac82
Maximus Apen <maximus.apen@sofico.be>
2025-06-01 19:16:56 +0100

    Refactor run_iperf.sh to improve argument handling for server and client modes

--- iperf/imageCreate/iperf/run_iperf.sh
+++ iperf/imageCreate/iperf/run_iperf.sh
@@ -1,12 +1,4 @@
-#!/bin/bash -e
-# ******************************************************************************
-# * Licensed Materials - Property of IBM
-# * , 5737-D43
-# * (C) Copyright IBM Corp. 2018,2019 All Rights Reserved.
-# * US Government Users Restricted Rights - Use, duplication or
-# * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-# ******************************************************************************
-#
+#!/bin/bash
 
 # By default run in server mode - but if the -c argument is passed, then just use the arguments supplied
 client_mode=false
