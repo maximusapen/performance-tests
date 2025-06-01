530516fb43935aa55708687e5eee95fb3fd2dccd
Maximus Apen <maximus.apen@sofico.be>
2025-06-01 19:00:50 +0100

    Update copyright information across multiple scripts to reflect new ownership by Maximus Apen, 2025. This change affects various scripts in the performance tests and tools directories, ensuring compliance with updated copyright standards.

--- tools/vpc-lite/run_iperf_suite.sh
+++ tools/vpc-lite/run_iperf_suite.sh
@@ -2,7 +2,7 @@
 # ******************************************************************************
 # * Licensed Materials - Property of IBM
 # * , 5737-D43
-# * (C) Copyright IBM Corp. 2019 All Rights Reserved.
+# * (C) Copyright Maximus Apen, 2025 All Rights Reserved.
 # * US Government Users Restricted Rights - Use, duplication or
 # * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
 # ******************************************************************************
